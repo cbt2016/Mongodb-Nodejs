14284dab8dff18e6545ba2c9d86cf09bbc4b0d10
cbt2016 <simotouil88@gmail.com>
2017-03-29 15:02:44 +0200

    GET request usin express

--- server/server.js
+++ server/server.js
@@ -19,6 +19,14 @@ app.post('/todos',(req,res)=>{
     });
 });
 
+app.get('/todos',(req,res)=>{
+   Todo.find({}).then((todos)=>{
+       res.send({todos});
+   },(err)=>{
+       res.status(400).send(e);
+   }); 
+});
+
 app.listen(port,()=>{
     console.log(`server listening on port ${port}`);
 });
